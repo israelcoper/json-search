f70d1fef36ecc15b96c85324adf32cf43ab5d640
Israel B. Coper <israelbcoper@gmail.com>
2024-02-11 20:52:41 +0800

    Setup json-search-cli

---
+++ lib/json/search.rb
@@ -0,0 +1,8 @@
+require "json/search/version"
+
+module Json
+  module Search
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/json/search_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Json::Search do
+  it "has a version number" do
+    expect(Json::Search::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
