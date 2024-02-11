fec1dfdeef1ba6278701764473db21f7b5beac00
Israel B. Coper <israelbcoper@gmail.com>
2024-02-11 20:58:06 +0800

    Implement search by keyword method

--- lib/json/search.rb
+++ lib/json/search.rb
@@ -1,8 +1,29 @@
 require "json/search/version"
+require 'json'
+require 'pry'
 
 module Json
-  module Search
+  class Search
     class Error < StandardError; end
-    # Your code goes here...
+
+    def search(keyword)
+      collection.select {|hash| hash["full_name"]&.downcase =~ /#{keyword}/}
+    end
+
+    private
+
+    def collection
+      parsed_json["data"]
+    end
+
+    def parsed_json
+      @parsed_json ||= begin
+        path = File.join(File.join(Dir.pwd, '/data/clients.json'))
+        file = File.read path
+        json = JSON.parse file
+
+        json
+      end
+    end
   end
 end
--- lib/json/search/version.rb
+++ lib/json/search/version.rb
@@ -1,5 +1,5 @@
 module Json
-  module Search
+  class Search
     VERSION = "0.1.0"
   end
 end
